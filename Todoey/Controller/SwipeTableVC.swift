a3e3d5c4fd729fd3f985256a05e6899bb7f93e73
R. Kukuh <rkukuh@gmail.com>
2018-01-09 21:28:09 +0700

    Create cocoa touch file to inherit SwipeTableViewController

---
+++ Todoey/Controller/SwipeTableVC.swift
@@ -0,0 +1,18 @@
+//
+//  SwipeTableVC.swift
+//  Todoey
+//
+//  Created by R. Kukuh on 09/01/18.
+//  Copyright © 2018 R. Kukuh. All rights reserved.
+//
+
+import UIKit
+import SwipeCellKit
+
+class SwipeTableVC: UITableViewController, SwipeTableViewCellDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
